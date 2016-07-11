89a4464bc514abe4e4056e944be1c5c2ba1b8932
twinklefingers <B1-66-ER@Rays-MBP.hsd1.mn.comcast.net>
2016-07-10 22:01:29 -0500

    Adding script file

---
+++ script.js
@@ -0,0 +1,10 @@
+function splinter(a,b){
+	var z;
+	z = a * b;
+	var array = [a, b, z];
+	return array;
+}
+
+/* I created this function in like 5 minutes,
+so there definitely must be something wrong 
+with it.. But it appears to work! */
